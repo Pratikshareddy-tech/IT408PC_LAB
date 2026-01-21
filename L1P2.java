edfbaf628607bc6bf97cb42c26d2fc5a6bf4ff54
Pratikshareddy-tech <pratikshareddy765@gmail.com>
2026-01-21 15:49:49 +0530

    Add files via upload

---
+++ L1P2.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class L1P2{
+ public static void main(String[] args){
+	int X=10;
+        double d=X;
+        System.out.println("Integer Value:" +X +"Double Value:"+X);
+        int x=(int)d;
+        System.out.println("Integer Value:" +x +"Double Value:"+x);
+
+}}
---
+++ L1P3.java
@@ -0,0 +1,11 @@
+import java. lang.*;
+class L1P3{
+	public static void main(String[] args){
+	int X=10;
+	float Y=20.9;
+	double D=30.88;
+	char ch='s';
+	short S=100;
+	long l=1000;
+	Boolean B=true;
+	System.out.println("integer:"+X +\n"float:"+Y +\n"double:"+D +\n"character:+ch +\n"short:"+S +\n"long:+l +"Boolean:+B +)}}
