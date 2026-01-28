9ec816da1489fddeadd51abebb146bece93fb98e
Bogale Pratiksha Reddy <pratikshareddy765@gmail.com>
2026-01-28 20:57:51 +0530

    Add files via upload

---
+++ LAB 1/L1P5.java
@@ -0,0 +1,12 @@
+
+class L1P5 {
+    public static void main(String[] args) {
+                
+        int age = 25;       
+        String name = "pratiksha"; 
+        double salary = 90000.50;    
+        System.out.println("Age: " + age);          
+        System.out.println("Name: " + name);        
+        System.out.println("Salary: " + salary);
+    }
+}
